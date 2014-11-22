527f3f8ce123370e0a90bd2f68983be2b8e5f604
David Konerding <dakoner@gmail.com>
2014-11-21 16:53:24 -0800

    Remove the web server; we can access local content with a single method call to WebSettings.

--- app/src/main/java/com/google/apps/android/d3webview/MainActivity.java
+++ app/src/main/java/com/google/apps/android/d3webview/MainActivity.java
@@ -1,17 +1,9 @@
 package com.google.apps.android.d3webview;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
-
 
 public class MainActivity extends Activity {
     WebView mWebView;
@@ -21,13 +13,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Context context = getApplicationContext();
-        context.startService(new Intent(context, HTTPService.class));
-
         final WebView mWebView = (WebView) findViewById(R.id.webView);
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        mWebView.loadUrl("http://localhost:8080/index.html");
-
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        mWebView.loadUrl("file:///android_asset/static_html/index.html");
     }
 }
