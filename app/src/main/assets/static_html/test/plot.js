b2f39233996f4242a33b45718e19e2c333f30717
David Konerding <dakoner@gmail.com>
2014-11-27 08:38:18 -0800

    extra files

---
+++ app/src/main/assets/static_html/test/plot.js
@@ -0,0 +1,104 @@
+// TODO(dek): wrap vars in a struct
+// TODO(dek): merge conversion into filter
+// TODO(dek): pass in conversion function (make it functional, construct a new data object to plot instead of reusing data.)
+// TODO(dek): pass in nesting key as function
+
+
+function callback(variable, margin, x, y, xAxis, yAxis, line, svg, width, height, error, data) {
+    if(error) {return console.warn(error)};
+    data = data.weatherdata;
+    data.forEach(function(d) {
+	    d.date = new Date(d.created_at);
+	    d[variable] = parseFloat(d[variable]);
+    });
+    //data = data.filter(function(d) {
+    //   if (d[variable] > 90) return false;
+    //	    return true;
+    //});
+
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain(d3.extent(data, function(d) { return d[variable]; }));
+
+    var dataNest = d3.nest()
+        .key(function(d) { return d.uuid; })
+        .entries(data);
+
+    var color = d3.scale.category10();
+    legendSpace = width/dataNest.length; // spacing for legend
+
+    dataNest.forEach(function(d, i) {
+        svg.append("path")
+            .attr("class", "line")
+            .style("stroke", function() {
+                            return d.color = color(d.key); })
+            .attr("d", line(d.values));
+
+        svg.append("text")
+            .attr("x", (legendSpace/2)+i*legendSpace) // spacing
+            .attr("y", height + (margin.bottom/2)+ 25)
+            .attr("class", "legend")
+            .style("fill", function() {
+                return d.color = color(d.key); })
+            .text(d.key);
+    });
+
+	svg.append("g")
+    	.attr("class", "x axis")
+    	.attr("transform", "translate(0," + height + ")")
+    	.call(xAxis)
+        .selectAll("text")
+          .style("text-anchor", "end")
+          .attr("dx", "-.8em")
+          .attr("dy", ".15em")
+          .attr("transform", function(d) {
+            return "rotate(-90)"
+          }
+        );
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+}
+
+
+function line_chart(url, variable) {
+
+
+var margin = {top: 25, right: 15, bottom: 120, left: 30},
+    width = window.innerWidth - margin.left - margin.right,
+    height = window.innerHeight - margin.top - margin.bottom;
+
+var x = d3.time.scale()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .ticks(d3.time.days, 1)
+    .tickFormat(d3.time.format("%Y-%m-%d"));
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var line = d3.svg.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d[variable]); })
+    .defined(function(d) {
+       return d.date != null && d.date != undefined &&
+              d[variable] != null && d[variable] != undefined && !isNaN(d[variable])});
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+d3.json(url, function(error, data) {
+  callback(variable, margin, x, y, xAxis, yAxis, line, svg, width, height, error, data);
+  });
+}
